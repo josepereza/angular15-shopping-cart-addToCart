91e88d419b3ffe675914f4fcbd34df2f58518aad
josepereza <josepereza66@gmail.com>
2023-03-06 18:41:14 +0100

    agregado this.cartnumberfunc

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Producto } from 'src/app/interfaces/producto';
 import { CartService } from 'src/app/services/cart.service';
 @Component({
@@ -6,11 +6,12 @@ import { CartService } from 'src/app/services/cart.service';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
   constructor(private cartService: CartService) {}
   ngOnInit(): void {
     this.CartDetails();
     this.loadCart();
+    this.cartNumberFunc();
   }
 
   getCartDetails: Producto[] = [];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Producto } from 'src/app/interfaces/producto';
 import { CartService } from 'src/app/services/cart.service';
 @Component({
@@ -6,50 +6,57 @@ import { CartService } from 'src/app/services/cart.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   constructor(private cartService:CartService){
 
   }
-productArray=[
+  ngOnInit(): void {
+    this.cartNumberFunc();
+
+  }
+productAlmacen=[
   {
     prodId:1, 
     img:'./../../assets/images/t1.jpg',
    precio:400,
-   qty:1
+   
 },
 {
   prodId:2,
   img:'./../../assets/images/t2.jpg',
    precio:300,
-   qty:1
+  
 },
 {
   prodId:3,
   img:'./../../assets/images/t3.jpg',
    precio:400,
-   qty:1
+  
 },
 {
   prodId:4,
   img:'./../../assets/images/t4.jpg',
    precio:400,
-   qty:1
+   
 }
 
 ]
+productArray=this.productAlmacen.map(prod=>{ return {...prod,qty:1}});
+
 
 inc(prod:Producto){
-prod.qty +=1
+prod.qty! +=1
 }
 dec(prod:Producto){
   if(prod.qty !=1){
-      prod.qty -=1
+      prod.qty! -=1
 
   }
 
 }
 itemsCart:any[] = [];
   addCart(category:Producto){
+    console.log('esto es en addCart', this.productArray)
     let cartDataNull = 
   localStorage.getItem('localCart');
     if(cartDataNull == null){
@@ -73,7 +80,7 @@ if(id ===
 parseInt(this.itemsCart[i].prodId))
 {
         this.itemsCart[i].qty = 
-category.qty;
+category.qty
           index = i;
           break;
         }
@@ -94,6 +101,7 @@ this.itemsCart.push(category);
   }
 
  cartNumber:number = 0;
+
  cartNumberFunc(){
    var cartValue = 
 JSON.parse
